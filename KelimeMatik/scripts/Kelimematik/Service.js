22f5bdada69db9ffd6e8ea172856ab0a13ebcb39
ufuksahin@gmail.com <ufuksahin@gmail.com>
2013-03-10 10:02:13 -0500

    Service

--- KelimeMatik/scripts/Kelimematik/Service.js
+++ KelimeMatik/scripts/Kelimematik/Service.js
@@ -1,4 +1,34 @@
-var GetQuestions = function(questionCount)
+var ApiAddress = "http://kelimematik.cloudapp.net/api/Words/";
+
+var GetQuestions = function(questionCount, callback)
 {
-    var parameters = string.Format("GetQuestions?questionCount={0}&deviceID={1}", questionCount, SystemInfo.deviceUniqueIdentifier);  
+    var parameters = "{'questionCount':'" + questionCount + "','deviceID':'" + 1 + "'}";
+
+    $.ajax({
+        type: "POST",
+        url: ApiAddress + "GetQuestions",
+        parameters: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: callback,
+        error: callback
+    });
 };
+
+var SendResult = function(results)
+{
+    var parameters = "{'GetJsonResults':'" + $.toJSON(results) + "','DeviceID':'" + 1 + "'}";
+    
+    alert(parameters);
+    
+    $.ajax({ 
+        type: "POST",
+        url: ApiAddress + "SendResults",
+        parameters: parameters,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: callback,
+        error: callback
+    });
+}
+
